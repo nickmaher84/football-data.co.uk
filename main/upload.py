87bf176415759c25967e272b7656706ce486e956
nickmaher84 <Anfield1>
2022-03-18 18:15:13 +0100

    Add upload.py to load match data to database

---
+++ main/upload.py
@@ -0,0 +1,61 @@
+from main.database import engine
+from main.schema import matches
+
+import pandas as pd
+import numpy as np
+
+match_headers = {
+    'Date': 'date',
+    'Time': 'time',
+    'Div': 'division',
+    'Country': 'country',
+    'League': 'league',
+    'Season': 'season',
+    'HT': 'home_team',
+    'Home': 'home_team',
+    'HomeTeam': 'home_team',
+    'AT': 'away_team',
+    'Away': 'away_team',
+    'AwayTeam': 'away_team',
+    'HG': 'home_goals',
+    'FTHG': 'home_goals',
+    'AG': 'away_goals',
+    'FTAG': 'away_goals',
+    'Res': 'result',
+    'FTR': 'result',
+    'HTHG': 'ht_home_goals',
+    'HTR': 'ht_result',
+    'HTAG': 'ht_away_goals',
+    'Referee': 'referee',
+    'Attendance': 'attendance',
+}
+
+
+def upload_file(url):
+    print('Loading data from {url}'.format(url=url))
+
+    df = pd.read_csv(
+        url,
+        encoding='cp1252',
+        parse_dates=['Date'],
+        dayfirst=True,
+        on_bad_lines=lambda x: x,
+        engine='python',
+    ).dropna(
+        axis=1,
+        how='all'
+    ).dropna(
+        subset=['Date'],
+    )
+
+    subset = [column for column in df.columns if column in match_headers]
+    match_data = df[subset].rename(columns=match_headers)
+    match_data = match_data.apply(lambda x: x.str.strip() if x.dtype == 'object' else x)
+    match_data['url'] = url
+
+    engine.execute(
+        matches.delete().where(matches.c.url == url)
+    )
+    engine.execute(
+        matches.insert().values(match_data.replace({np.nan: None}).to_dict('records'))
+    )
