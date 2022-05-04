e923862954cda748b15bc627a3833a6f9264c779
Nick Maher <nickmaher84@users.noreply.github.com>
2022-05-04 12:11:11 +0200

    Add __init__.py for loader and dbt

--- loader/alembic/env.py
+++ loader/alembic/env.py
@@ -2,6 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from database import metadata
 
 from alembic import context
 
@@ -16,7 +17,6 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from database import metadata
 target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
