35f4d5e9a81fbf39152cab8dac7e9e531cc8cb6c
Nick Maher <nickmaher84@users.noreply.github.com>
2022-05-26 21:22:30 +0200

    Fix alembic version autogeneration

--- loader/alembic/env.py
+++ loader/alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from database import metadata
+import schema
 
 from alembic import context
 
@@ -17,7 +17,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-target_metadata = metadata
+target_metadata = schema.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
